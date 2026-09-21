75b2f2637ab57f16ddbb16bec8fe74dcebfb3152
mbahnunung <mbahnunung@users.noreply.github.com>
2026-09-21 21:32:56 +0700

    Update zenov2.js

--- js/zenov2.js
+++ js/zenov2.js
@@ -1,7 +1,7 @@
 const RADIO_NAME = 'mbah nunung Online Live';
 
 // Change Stream URL Here, Supports, ICECAST, ZENO, SHOUTCAST, RADIOJAR and any other stream service.
-const URL_STREAMING = 'https://stream.zeno.fm/duauceloe8bvv';
+const URL_STREAMING = 'https://scure.streaming.id:8000/mbahnunungonline';
 
 // You can find the mount point in the Broadcast Settings.
 // To generate the Zeno Radio API link from the mount point,
